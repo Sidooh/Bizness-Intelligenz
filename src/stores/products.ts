6c9de75250fc49772446cb2bc1ea1179ae1b3a1f
Nabcellent <nabcellent.dev@gmail.com>
2023-08-19 14:56:47 +0300

    Implements:  - Suspense & Error Fallback  - Activation Metrics  - Types & Enums

--- src/stores/products.ts
+++ src/stores/products.ts
@@ -4,15 +4,15 @@ import axios from "@/utils/axios.ts";
 import { CONFIG } from "@/config.ts";
 
 export const useProductsStore = defineStore('products', {
+    state: () => ({
+        baseUrl: CONFIG.sidooh.services.products.api.url,
+        transactions: <Transaction[] | undefined>undefined
+    }),
     actions: {
         async fetchTransactions() {
             const { data: res } = await axios.get(`${this.baseUrl}/transactions?page_size=20`)
 
             this.transactions = res.data.data
         }
-    },
-    state: () => ({
-        baseUrl: CONFIG.sidooh.services.products.api.url,
-        transactions: <Transaction[] | undefined>undefined
-    })
+    }
 })
