a896a4a1978b6162c1acee486667e3d88e7e27a7
Nabcellent <nabcellent.dev@gmail.com>
2023-08-22 05:22:02 +0300

    Updates env declaration file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -11,7 +11,12 @@ interface ImportMetaEnv {
     readonly VITE_PRODUCTS_API_URL: string
 
     readonly VITE_ACCOUNTS_DASHBOARD_URL: string
+    readonly VITE_ENTERPRISE_DASHBOARD_URL: string
+    readonly VITE_PAYMENTS_DASHBOARD_URL: string
     readonly VITE_PRODUCTS_DASHBOARD_URL: string
+    readonly VITE_NOTIFY_DASHBOARD_URL: string
+    readonly VITE_SAVINGS_DASHBOARD_URL: string
+    readonly VITE_USSD_DASHBOARD_URL: string
 }
 
 interface ImportMeta {
